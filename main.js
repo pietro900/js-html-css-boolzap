087f5fb31a925b400d9e0de1402c05775a439651
pietro <pietro1990@hotmail.it>
2020-05-14 08:11:33 +0200

    inserito drop

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+//intercetto il click sull'icona del dropdowen;
+$('.fa-angle-down').click(function () {
+    //recupero il tag ul con classe dropdown che è suo fratello
+    var sottomenu = $(this).next('.dropdown');
+    //valuto se l'elemento su cui ho cliccato ha gia il dropdawn aperto;
+    if(sottomenu.is(':visible')){
+        //se si allora ho cliccato lastessa voce per chiuderlo;
+        sottomenu.hide();
+    } else {
+        //se no allora ho cliccato una nuova voce per aprirlo;
+        //chiudo gli altri eventuali aperti in precedenza;
+        $('.dropdown').hide();
+        //visuallizzo il menu dropdown
+        sottomenu.show();
+    }
+})
+
+
 //intercetto il click sull' untente selezionato;
 $('.utente').click(function(){
 
